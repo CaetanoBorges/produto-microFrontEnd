1f8e254a3827981e4de2383857d5e189a4ae2acb
cbcaetanoborges@gmail.com <cbcaetanoborges@gmail.com>
2022-03-29 21:06:00 +0100

    primeiro

---
+++ index.js
@@ -0,0 +1,14 @@
+function debliw_preciso(){
+  $('.debliw-quero-form').toggle('slow');
+}
+
+$('.debliw-quero-action').click(function(){
+  
+  console.log(1234566789);
+  
+  var nome = $('.debliw-form-nome').val();
+  var telefone = $('.debliw-form-telefone').val();
+  var email = $('.debliw-form-email').val();
+  
+  
+});
